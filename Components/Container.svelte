cdc7676c152c488fb1fd14c7ba4e5b2be44a30c3
egeres <egeres.default@gmail.com>
2023-11-11 13:43:31 +0100

    feat: Svelte timeline is scrollable

--- Components/Container.svelte
+++ Components/Container.svelte
@@ -1,18 +1,15 @@
 <script>
-    // You can add script logic here if needed
-    import Timeline from './Timeline.svelte';
+	import Timeline from "./Timeline.svelte";
 </script>
 
-<style>
-.container {
-    width: 100%;
-    height: 100%;
-    /* Add more styling if needed */
-    background-color: rgb(52, 19, 19);
-}
-</style>
-
 <div class="container">
-<!-- <slot></slot> This is where child components will be rendered -->
-<Timeline/>
+	<Timeline />
 </div>
+
+<style>
+	.container {
+		width: 100%;
+		height: 100%;
+		background-color: rgb(52, 19, 19);
+	}
+</style>
--- Components/Timeline.svelte
+++ Components/Timeline.svelte
@@ -1,30 +1,113 @@
 <script>
-    // Script logic can go here if needed
-  </script>
-  
-  <style>
-    .timeline {
-      width: 100%;
-      height: 300px;
-      position: relative;
-      /* Add more styling for the timeline here */
-    }
-  
-    .tick-mark {
-        position: absolute;
-        bottom: 0;
-        height: 50px;
-        width: 0;
-        border-left: none;
-        background-color: transparent;
-        box-shadow: 0px 0px 0px 0.5px white;
-    }
-  </style>
-  
-  <div class="timeline">
-    <!-- Generate tick marks for each month -->
-    {#each Array(12) as _, index}
-      <div class="tick-mark" style="left: {index * 100 / 11}%;">{index + 1}</div>
-    {/each}
-  </div>
-  
+	let pixelsPerYear = 300;
+	let zoomLevel = 1;
+	let isDragging = false;
+	let startX;
+
+	function startDrag(event) {
+		isDragging = true;
+		startX = event.clientX;
+		event.preventDefault();
+	}
+
+	function endDrag(event) {
+		isDragging = false;
+	}
+
+	function handleMouseMove(event) {
+		if (isDragging) {
+			const timeline = document.querySelector(".timeline");
+			const dx = event.clientX - startX;
+			timeline.scrollLeft -= dx;
+			startX = event.clientX;
+		}
+	}
+
+	function handleScroll(event) {
+		const timeline = document.querySelector(".timeline");
+		timeline.scrollLeft += event.deltaY;
+		event.preventDefault();
+	}
+
+	function handleZoom(event) {
+		if (event.ctrlKey) {
+			// zoomLevel += event.deltaY * -0.01;
+			// zoomLevel = Math.min(Math.max(.125, zoomLevel), 4);
+			// pixelsPerYear = 300 * zoomLevel;
+
+			pixelsPerYear += 20 * Math.sign(event.deltaY);
+			pixelsPerYear = Math.min(Math.max(200, pixelsPerYear), 400);
+		} else {
+			handleScroll(event);
+		}
+		event.preventDefault();
+	}
+</script>
+
+<div
+	class="timeline"
+    role="presentation"
+	on:wheel={handleZoom}
+	on:mousedown={startDrag}
+	on:mouseup={endDrag}
+	on:mouseleave={endDrag}
+	on:mousemove={handleMouseMove}
+>
+	{#each Array(10) as _, index (index)}
+		<!-- 5 years for demo, adjust as needed -->
+		<div class="tick-mark" style="left: {pixelsPerYear * index}px;">
+			{#if index === 7}
+				<!-- Assuming the current year is in the middle -->
+				<div class="current-date">
+					{new Date().toISOString().slice(0, 10)}
+				</div>
+			{/if}
+			<div class="year-label">
+				{new Date().getFullYear() - 7 + index}
+			</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.timeline {
+		width: 100%;
+		height: 300px;
+		position: relative;
+		/* Add more styling for the timeline here */
+		overflow-x: auto; /* Enables horizontal scrolling */
+		white-space: nowrap; /* Keeps the timeline in one line */
+
+		/* Hide the scrollbar */
+		-ms-overflow-style: none; /* IE and Edge */
+		scrollbar-width: none; /* Firefox */
+
+		/* Hide scrollbar for Chrome, Safari and Opera */
+	}
+
+	.timeline::-webkit-scrollbar {
+		display: none;
+	}
+
+	.tick-mark {
+		position: absolute;
+		bottom: 0;
+		height: 50px;
+		width: 2px;
+		background-color: white;
+	}
+
+	.current-date {
+		writing-mode: vertical-rl;
+		transform-origin: bottom;
+		position: absolute;
+		transform: translateX(0%) translateY(-100%);
+	}
+
+	.year-label {
+		writing-mode: vertical-rl;
+		transform-origin: bottom;
+		position: absolute;
+		bottom: 0;
+	}
+</style>
